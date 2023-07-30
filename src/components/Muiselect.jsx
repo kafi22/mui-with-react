38a4f6f544568e18e5277892f1b14c35ce1614ef
Your Name <you@example.com>
2023-07-30 18:26:43 +0600

    select items

--- src/components/Muiselect.jsx
+++ src/components/Muiselect.jsx
@@ -1,12 +1,24 @@
 import { Box, MenuItem, TextField } from "@mui/material"
+import { useState } from "react"
 
 const Muiselect = () => {
 
+    const [value, setValue] = useState([])
+
+
+    console.log(value);
 
     return (
         <>
         <Box width='250px' >
-        <TextField label='select country' select>
+        <TextField label='select country' 
+        fullWidth select
+         value={value}
+          onChange={e => setValue(e.target.value)}
+          SelectProps={{
+            multiple : true
+          }}
+          >
             <MenuItem value='India'>India</MenuItem>
             <MenuItem value='Bangladesh'>Bangladesh</MenuItem>
             <MenuItem value='SriLanka'>SriLanka</MenuItem>
