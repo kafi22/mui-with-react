5240c543dc7289d4b2a4faeb560b90ce190fd7a5
Your Name <you@example.com>
2023-08-04 23:55:41 +0600

    add-sidebar-component

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -22,7 +22,7 @@ const Herosection = () => {
       <Stack marginTop={'5rem'}>
         <Grid container justifyContent={'space-between'} spacing={3}>
           <Grid item xs={12} md={6}>
-          <Card variant='outlined' sx={{display : 'flex'}}>
+          <Card variant='outlined' sx={{display : 'flex', boxShadow : '1'}}>
             <Box sx={{display : 'flex', justifyContent : 'space-between', flexDirection : 'column'}}>
             <CardContent>
               <Typography variant='h5' component={'h3'} gutterBottom>
@@ -41,12 +41,12 @@ const Herosection = () => {
               </Typography>
             </CardContent>
             </Box>
-            <CardMedia component='img' image={cardImg} sx={{width : '150px'}}  />
+            <CardMedia component='img' image={cardImg} sx={{width : '150px', objectFit : 'cover'}}  />
           </Card>
           </Grid>
 
           <Grid item xs={12} md={6}>
-          <Card variant='outlined' sx={{display : 'flex'}}  >
+          <Card variant='outlined' sx={{display : 'flex', boxShadow : '1'}}  >
             <Box sx={{display :'flex', flexDirection : 'column', justifyContent : 'space-between'}}>
             <CardContent>
               <Typography variant='h5' component={'h3'} gutterBottom>
@@ -65,7 +65,10 @@ const Herosection = () => {
               </Typography>
             </CardContent>
             </Box>
-            <CardMedia component={'img'} image={cardImg} sx={{width : '150px'}} />
+            <CardMedia component={'img'} 
+            image={cardImg} 
+            sx={{width : '150px', objectFit : 'cover',}} 
+            />
           </Card>
           </Grid>
         </Grid>
