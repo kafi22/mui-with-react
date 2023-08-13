02eb5f0fd0a2432bc98de23f8092968e2199d24a
Your Name <you@example.com>
2023-08-13 19:21:39 +0600

    add: more-feature

--- src/components/Muiupload.jsx
+++ src/components/Muiupload.jsx
@@ -1,76 +1,139 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Stack, Box, Button, Card, Typography } from '@mui/material';
 import { FileUpload } from '@mui/icons-material';
 
-const Muiupload = () => {
+const Muiupload = ({
+    register,
+    uploadFiles,
+    fileUploadName
+}) => {
 
-    const [uploadFiles, setUploadFile] = useState(null)
+    const [uploadFile, setUploadFile] = useState(null)
 
-    const getTheFile = () => {
+  
 
-        document.getElementById("uploadFile").click()
-    };
+    const getTheFileUpload = (fileUpload) => {
 
-    const getUploadsFile = ({file}, status) => {
+        document.getElementById(fileUpload).click()
+    }
 
-        if(status === "status") {
+    const getUploadsFiles = ({file}, status) => {
+        if(status === "done") {
             setUploadFile(file)
+            uploadFiles(file)
         }
+    }
 
-    };
-
-
-  return (
-
-    <Stack component={"div"} sx={{display : "flex", flexDirection : "column", justifyContent : "flex-start", alignItems : "flex-start", height : "100%"}}>
-        <Box sx={{display : "flex", flexDirection : "column", justifyContent : "flex-start", alignItems : "flex-start", height : "100%", gap : "2rem"}}>
-
-            <Box sx={{display : "flex", flexDirection : "row", justifyContent : "flex-start", width : "100%"}}>
-
-                <Button variant='contained' 
-                type='button' 
-                endIcon={ <FileUpload />}
-                onClick={getTheFile}
+    return (
+            <Stack
+                component={"div"}
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "flex-start",
+                    alignItems: "flex-start",
+                    height: "100%",
+                }}
+            >
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "flex-start",
+                        alignItems: "flex-start",
+                        height: "100%",
+                        gap: "2rem",
+                    }}
                 >
-                    Upload here
-                </Button>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "flex-start",
+                            width: "100%",
+                        }}
+                    >
+                        <Button
+                            variant='contained'
+                            type='submit'
+                            endIcon={<FileUpload />}
+                            onClick={() => getTheFileUpload('fileUpload')}
+                        >
+                            Upload here
+                        </Button>
 
-                <input 
-                hidden
-                type='file'
-                id='uploadFile'
-                onChange={e => getUploadsFile({ file : e.target.files[0]}, "status")}
-                accept="image/*, .pdf, .doc, .docx"
-                
-                />
+                        <input
+                            hidden
+                            type='file'
+                            id='fileUpload'
+                            {...register(fileUploadName)}
+                            accept="image/*, .pdf, .doc, .docx"
+                            onChange={e => getUploadsFiles({file : e.target.files[0]}, "done")}
+                        />
+                    </Box>
 
-            </Box>
+                    {uploadFile && (
+                        <Box
+                            sx={{
+                                display: "flex",
+                                justifyContent: "flex-start",
+                                flexDirection: "column",
+                                alignItems: "flex-start",
+                                gap: "1rem",
+                            }}
+                        >
+                            {uploadFile.type.includes("image") ? (
+                                <Box
+                                    sx={{
+                                        display: "flex",
+                                        justifyContent: "flex-start",
+                                        flexDirection: "row",
+                                        alignItems: "flex-start",
+                                        gap: "1rem",
+                                    }}
+                                >
+                                    <Card>
+                                        <img
+                                            src={URL.createObjectURL(uploadFile)}
+                                            style={{
+                                                height: "120px",
+                                                width: "120px",
+                                                objectFit: "cover",
+                                            }}
+                                            alt=""
+                                        />
+                                    </Card>
 
-            {uploadFiles && (
-                <Box sx={{display : "flex", justifyContent : "flex-start", flexDirection : "column", alignItems : "flex-start", gap : "1rem"}}>
-                    {uploadFiles.type.includes("image") ? (
-                        <Box sx={{display : "flex", justifyContent : "flex-start", flexDirection : "row", alignItems : "flex-start", gap : "1rem"}}>
-                            <Card>
-                                <img src={URL.createObjectURL(uploadFiles)} style={{height : "120px", width : "120px", objectFit : "cover"}} alt="" />
-                            </Card>
-
-                            <Typography variant='body2' component={"p"}>
-                               image Name : {uploadFiles.name}
-                            </Typography>
+                                    <Typography
+                                        variant='body2'
+                                        component={"p"}
+                                    >
+                                        Image Name: {uploadFile.name}
+                                    </Typography>
+                                </Box>
+                            ) : (
+                                <Box
+                                    sx={{
+                                        display: "flex",
+                                        justifyContent: "flex-start",
+                                        flexDirection: "row",
+                                        alignItems: "flex-start",
+                                    }}
+                                >
+                                    <Typography
+                                        variant='body2'
+                                        component={"p"}
+                                    >
+                                        File Name: {uploadFile.name}
+                                    </Typography>
+                                </Box>
+                            )}
                         </Box>
-                        
-                    ) : (
-                        <Box sx={{display : "flex", justifyContent : "flex-start", flexDirection : "row", alignItems : "flex-start"}}>
-                            <Typography variant='body2' component={"p"}>
-                              File Name : {uploadFiles.name}
-                            </Typography>
-                        </Box>
-                    ) }
+                    )}
                 </Box>
-            )}
-        </Box>
-    </Stack>
-  )
-}
+            </Stack>
+    );
+};
 
-export default Muiupload
+export default Muiupload;
