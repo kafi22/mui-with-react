de3ef5b13175514564900e91c59d899026cb923c
Your Name <you@example.com>
2023-08-16 23:08:41 +0600

    add: Business-page

--- src/Pages/Business.jsx
+++ src/Pages/Business.jsx
@@ -1,16 +1,40 @@
-import { Typography } from '@mui/material'
 import React from 'react'
+import { Typography, Card, CardContent, CardMedia, Stack, Box } from '@mui/material'
 import { Link } from 'react-router-dom'
+import { CardActionArea } from '@mui/material';
+import cardImg from "../assets/image/maddy-weiss-V9EfIYUuGeo-unsplash.jpg"
 
 const Business = () => {
   return (
-    <div>
-      <Typography>
-        this is Business page
-      </Typography>
 
-      <Link to={'/'}>Back to home</Link>
-    </div>
+
+<Stack component={"div"}>
+    <Card sx={{ maxWidth: 345 }}>
+    <CardActionArea>
+      <CardMedia
+        component="img"
+        height="140"
+        image={cardImg}
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+  </Card>
+
+  <Box mt={"2rem"}>
+  <Link to={'/'}>Back to home</Link>
+  </Box>
+</Stack>
+
+
   )
 }
 
